c5091cf64eaac573f21f012610291d3e8abd3c8e
lambda-snail <>
2025-01-24 21:50:14 +0100

    play with thread pool

--- source/networking/tcp_server.cpp
+++ source/networking/tcp_server.cpp
@@ -88,8 +88,8 @@ asio::awaitable<void> listener(
     tcp_acceptor_t acceptor(executor, {asio::ip::tcp::v4(), port});
 
     acceptor.set_option(asio::ip::tcp::acceptor::reuse_address(true));
-     typedef asio::detail::socket_option::boolean<SOL_SOCKET, SO_REUSEPORT> reuse_port;
-     acceptor.set_option(reuse_port(true));
+    typedef asio::detail::socket_option::boolean<SOL_SOCKET, SO_REUSEPORT> reuse_port;
+    acceptor.set_option(reuse_port(true));
 
     // int one = 1;
     // auto result = setsockopt(acceptor.native_handle(), SOL_SOCKET /*SOL_SOCKET*/, SO_REUSEADDR | SO_REUSEPORT, &one, sizeof(one));
@@ -124,7 +124,7 @@ public:
             signal_set.add(SIGQUIT);
 #endif
             signal_set.async_wait(
-                [this](std::error_code ec, int signal)
+                [&](std::error_code ec, int signal)
                 {
                     m_logger->get_system_logger()->
                             info("The system received signal {} - {}", signal, strsignal(signal));
@@ -133,31 +133,9 @@ public:
 
             asio::co_spawn(m_context, listener(port, dispatch, buffer_pool), asio::detached);
 
-            m_context.run();
-
-            // tcp_server server(m_context, port, dispatch, buffer_pool);
-            //
-            // // See https://think-async.com/Asio/asio-1.30.2/src/examples/cpp11/http/server3/server.cpp
-            // for (std::size_t i = 0; i < thread_pool_size_; ++i)
-            //     m_thread_pool.emplace_back([&] { m_context.run(); });
-
-            // sigset_t wait_mask;
-            // sigemptyset(&wait_mask);
-            // sigaddset(&wait_mask, SIGINT);
-            // sigaddset(&wait_mask, SIGQUIT);
-            // sigaddset(&wait_mask, SIGTERM);
-            // sigaddset(&wait_mask, SIGHUP);
-            // pthread_sigmask(SIG_BLOCK, &wait_mask, 0);
-            // int signal = 0;
-            // sigwait(&wait_mask, &signal);
-
-            //m_logger->get_system_logger()->info("The system received signal {} - {}", signal, strsignal(signal));
-
-            //m_context.stop();
-            // for (auto &thread: m_thread_pool)
-            // {
-            //     if (thread.joinable()) thread.join();
-            // }
+            // If we are using an asio::thread_pool, we should join here to wait for
+            // asio to be done (i.e., server shutdown)
+            // m_context.join();
         } catch (std::exception &e)
         {
             m_logger->get_system_logger()->error("Exception in server runner: {}", e.what());
@@ -170,10 +148,13 @@ public:
     }
 
 private:
-    asio::io_context m_context{ 1 };
-    std::vector<std::thread> m_thread_pool{};
+    size_t m_thread_pool_size = 8;
 
-    int8_t thread_pool_size_ = 8;
+    // We can use a single threaded context or a thread pool
+    // TODO: Both of these need to be tested and evaluated. Perhaps the usage can
+    // be a configuration at start?
+    asio::io_context m_context { 1 };
+    //asio::thread_pool m_context { m_thread_pool_size };
 
     std::shared_ptr<LambdaSnail::logging::logger> m_logger;
 };
